a56d056dcbc66ea2997160cbd7ea94f3aacf4ec8
Olga Grochowska <olga.programistka@gmail.com>
2022-06-13 19:57:12 +0200

    Form validation

--- script.js
+++ script.js
@@ -11,15 +11,23 @@ let shoppingList = [
 
 document.addEventListener('DOMContentLoaded', () => {
     ul = document.getElementById('shoppingList');
+    inputError = document.getElementById('inputError');
     newItemForm = document.getElementById('newItemForm');
 
     newItemForm.addEventListener('submit', (event) => {
         event.preventDefault();
         let input = event.target.elements[0];
-        
-        addListItem(input.value);
 
-        input.value = "";
+        if (input.value.length > 2 && !input.value.startsWith(' ')) {
+            addListItem(input.value);
+            input.value = ""
+            input.classList.remove('input-danger');
+            inputError.innerText = "";
+        } else {
+            inputError.innerText = "Nazwa nie spełnia kryteriów."
+            input.classList.add('input-danger');
+        }
+
     })
 
     for (let shoppingItem of shoppingList) {
